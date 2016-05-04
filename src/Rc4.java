e337d9c8418bd1c86114bc5440ee5232bc799caa
VRushLP <rmfarc@uw.edu>
2016-05-03 17:54:38 -0700

    Almost!

--- src/Rc4.java
+++ src/Rc4.java
@@ -9,19 +9,17 @@ public class Rc4 {
 
 	private static final short sLength = 256;
 	private static short[] S;
-	private static int[] key;
+	private static int[] key = new int[] { 1, 2, 3, 4, 5 };
 
 	// Comment in to encrypt
-	private static String inputPath = ".\\src\\King James Bible.txt";
-	private static String outputPath = ".\\src\\encrypted.txt";
+	// private static String inputPath = ".\\src\\Hello World.txt";
+	// private static String outputPath = ".\\src\\encrypted.txt";
 
 	// Comment in to decrypt
-	// private static String inputPath = ".\\src\\encrypted.txt";
-	// private static String outputPath = ".\\src\\decrypted.txt";
+	private static String inputPath = ".\\src\\encrypted.txt";
+	private static String outputPath = ".\\src\\decrypted.txt";
 
 	public static void main(String[] args) {
-		key = new int[] { 1, 2, 3, 4, 5 };
-
 		long start = System.currentTimeMillis();
 		System.out.println("Starting key scheduling algorithm.");
 		keyScheduling();
@@ -33,14 +31,14 @@ public class Rc4 {
 
 		try {
 			reader = new BufferedReader(new FileReader(toEncrypt));
-
 			// Iterate over file and add to plaintext buffer
 			System.out.println("Reading in file.");
-			String temp;
-			while ((temp = reader.readLine()) != null) {
-				plaintext.append(temp);
+			int temp;
+			while ((temp = reader.read()) != -1) {
+				plaintext.append((char) temp);
 			}
 			System.out.println("File read.");
+			System.out.println(plaintext.toString());
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -64,16 +62,18 @@ public class Rc4 {
 
 	private static String encrypt(final String plaintext) {
 		StringBuffer out = new StringBuffer();
-		byte[] bytes = plaintext.getBytes();
+		char[] chars = plaintext.toCharArray();
 
 		int i = 0, j = 0, k;
 		int length = plaintext.length();
-		for (int counter = 0; counter < length - 1; counter++) {
+		for (int counter = 0; counter < length; counter++) {
 			i = ((i + 1) % S.length);
 			j = ((j + S[i]) % S.length);
-			xorSwap(i, j);
+			S[i] ^= S[j];
+			S[j] ^= S[i];
+			S[i] ^= S[j];
 			k = S[(S[i] + S[j]) % S.length];
-			out.append(bytes[i] ^ k);
+			out.append((char) (chars[counter] ^ k));
 		}
 		return out.toString();
 	}
@@ -85,19 +85,12 @@ public class Rc4 {
 		S = new short[sLength];
 		for (int i = 0; i < S.length; i++)
 			S[i] = (short) i;
-
-		for (int j = 0, i = 0; i < S.length; i++) {
+		int j = 0;
+		for (int i = 0; i < S.length; i++) {
 			j = (j + S[i] + key[i % key.length]) % S.length;
-			xorSwap(i, j);
+			S[i] ^= S[j];
+			S[j] ^= S[i];
+			S[i] ^= S[j];
 		}
-	}
-
-	private static void xorSwap(int firstIndex, int secondIndex) {
-		S[firstIndex] ^= S[secondIndex];
-		S[secondIndex] ^= S[firstIndex];
-		S[firstIndex] ^= S[secondIndex];
-		// short temp = S[firstIndex];
-		// S[firstIndex] = S[secondIndex];
-		// S[secondIndex] = temp;
 	}
 }
